67f405a8dc27529bfaa8279d8170d000e5e21031
Weskimo <samuel.hughes4@btinternet.com>
2022-02-11 14:20:04 +0000

    added all screens as blank components

---
+++ Components/NotificationScreen.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text, Button, TextInput} from 'react-native';
+
+
+class NotificationScreen extends Component {
+    render(){
+      
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Sign Up</Text>
+          
+        
+        </View>
+      );
+    } 
+}
+export default NotificationScreen;
