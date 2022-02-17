7c124847aba5a1a6a01637dd3cfda700b7efe628
Weskimo <samuel.hughes4@btinternet.com>
2022-02-17 15:19:26 +0000

    got all the user info showing on the profile

--- Components/ProfileScreen.js
+++ Components/ProfileScreen.js
@@ -25,7 +25,9 @@ class ProfileScreen extends Component {
     
         this.state = {
           isLoading: true,
-          userId: ''
+          userId: '',
+          first_Name: '',
+          last_name: ''
          
           
           
@@ -34,11 +36,16 @@ class ProfileScreen extends Component {
       retrieveData = async () => {
         try {
             const id = await AsyncStorage.getItem('@user_id');
+            const firstName = await AsyncStorage.getItem('@first_name');
+            const lastName = await AsyncStorage.getItem('@last_name');
             // const data = JSON.stringify(jsonValue);
          
             this.setState({
                 userId: id,
-                isLoading: false
+                isLoading: false,
+                first_Name: firstName,
+                last_Name: lastName
+
             })
             console.log(value);
           
@@ -50,20 +57,58 @@ class ProfileScreen extends Component {
       
       
     componentDidMount(){
+        this.getProfileData();
         this.retrieveData();
+        
+    }
+
+    getProfileData = async () => {
+
+        const value = await AsyncStorage.getItem('@session_token');
+        const id = await AsyncStorage.getItem('@user_id');
+        return fetch("http://localhost:3333/api/1.0.0/user/" + id, {
+              'headers': {
+                'X-Authorization':  value
+              },
+            
+        })
+        .then((response) => {
+            if(response.status === 200){
+                
+                return response.json()
+               
+            }else if(response.status === 400){
+                throw 'Invalid email or password';
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .then(async (responseJson) => {
+                console.log(responseJson);
+                await AsyncStorage.setItem('@first_name', responseJson.first_name);
+                await AsyncStorage.setItem('@last_name', responseJson.last_name);
+
+                this.props.navigation.navigate("Home");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
     }
      
     render(){
         
         if(this.state.isLoading){
             return (
-                <View><Text>Loading... </Text></View>
+                <View><Text>Loading... 
+                    </Text></View>
             )
         }else{
             
             return (
                 <View>
                     <Text>Login id: {this.state.userId}</Text>
+                    <Text>First Name: {this.state.first_Name}</Text>
+                    <Text>Last Name: {this.state.last_Name}</Text>
                     
                 </View>
             )
