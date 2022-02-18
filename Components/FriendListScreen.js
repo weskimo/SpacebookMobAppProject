6287d7baa02a2896ccaee3b5f90ec43a312ca9a9
Weskimo <samuel.hughes4@btinternet.com>
2022-02-18 16:15:02 +0000

    implemented network requests for searchign new friends, showing user friend list, accept and deny requests, show outstanding requests in notis and request new friends

--- Components/FriendListScreen.js
+++ Components/FriendListScreen.js
@@ -1,30 +1,96 @@
 import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, SafeAreaView, View, Text, TextInput } from 'react-native';
-import SearchFriendsScreen from './SearchFriendsScreen';
+
+import { View, Text, Button, TextInput, FlatList, SafeAreaView, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 
 
 
 class FriendListScreen extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      listData: [],
+      requestId: ''
+    }
+
+  }
+
+  componentDidMount() {
+
+    this.getData();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  getData = async () => {
+    const value = await AsyncStorage.getItem('@session_token');
+    const id = await AsyncStorage.getItem('@user_id');
+    return fetch("http://localhost:3333/api/1.0.0/user/" + id  +"/friends", {
+          'headers': {
+            'X-Authorization':  value
+          }
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 401){
+              this.props.navigation.navigate("Login");
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .then((responseJson) => {
+          this.setState({
+            isLoading: false,
+            listData: responseJson
+          })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+  }
     render(){
         
-      return (
-      
-        <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-            <Text>Your friends:</Text>
-            
-        </View>
-      
-      );
+      if (this.state.isLoading){
+        return (
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text>Loading..</Text>
+          </View>
+        );
+      }else{
+        return (
+          <View>
+            <FlatList
+                  data={this.state.listData}
+                  renderItem={({item}) => (
+                      <View>
+                        <Text>
+                          {item.user_givenname} {item.user_familyname} {item.user_id.toString()}
+                          </Text>
+                          
+                          
+                      </View>
+                  )}
+                  keyExtractor={(item,index) => item.user_id.toString()}
+                />
+          </View>
+        );
+      }
     } 
 }
 export default FriendListScreen;
--- Components/NotificationScreen.js
+++ Components/NotificationScreen.js
@@ -1,27 +1,146 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { View, Text, Button, TextInput, FlatList, SafeAreaView, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FriendListScreen from './FriendListScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 
 const Tab = createBottomTabNavigator();
 
 class NotificationScreen extends Component {
-    render(){
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      listData: [],
+      requestId: ''
+    }
+
+  }
+
+  componentDidMount() {
+
+    this.getData();
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+
+
+  getData = async () => {
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/friendrequests", {
+          'headers': {
+            'X-Authorization':  value
+          }
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 401){
+              this.props.navigation.navigate("Login");
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .then((responseJson) => {
+          this.setState({
+            isLoading: false,
+            listData: responseJson
+          })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+  }
+
+
+  acceptFriend = async () => {
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/friendrequests/" + this.state.requestId  , {
+       method: 'post',
+       headers: {
+            'X-Authorization':  value
+          }
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 401){
+              this.props.navigation.navigate("Login");
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+  }
+
+  declineFriend = async () => {
+    const value = await AsyncStorage.getItem('@session_token');
+    return fetch("http://localhost:3333/api/1.0.0/friendrequests/" + this.state.requestId  , {
+       method: 'delete',
+       headers: {
+            'X-Authorization':  value
+          }
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 401){
+              this.props.navigation.navigate("Login");
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+  }
+
+  render(){
+       
       
+    if (this.state.isLoading){
       return (
         <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-          <Text>Notifications:</Text>
-          </View>
-    );
-      
-    } 
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>Loading..</Text>
+        </View>
+      );
+    }else{
+      return (
+        <View>
+          
+          <FlatList
+                data={this.state.listData}
+                renderItem={({item}) => (
+                    <View>
+                      <Text>
+                        {item.first_name} {item.last_name} {item.user_id.toString()}
+                        </Text>
+                        <Button title='Accept' onPress={() => {this.setState({requestId: item.user_id.toString()}); this.acceptFriend(); }}/>
+                        <Button title='Decline' onPress={() => {this.setState({requestId: item.user_id.toString()}); this.declineFriend(); }}/>
+                        
+                    </View>
+                )}
+                keyExtractor={(item,index) => item.user_id.toString()}
+              />
+        </View>
+      );
+    }
+}
 }
 export default NotificationScreen;
